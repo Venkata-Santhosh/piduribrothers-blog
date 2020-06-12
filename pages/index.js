cc39333800fec049da1846c4a36470f5298d96df
VenkataSanthosh <santhosh.piduri@gmail.com>
2020-06-12 19:29:43 -0400

    our blog initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import fs from 'fs';
 
-export default function Home() {
+const Home = ({posts}) => {
   return (
     <div className="container">
       <Head>
@@ -18,33 +20,15 @@ export default function Home() {
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {
+            posts.map(post => {
+             return( 
+                <Link key={post} href={"/blog/" + post} className="card">
+                  <a>{"/blog/" + post} &rarr;</a>
+                </Link>
+              );
+            })
+          }
         </div>
       </main>
 
@@ -206,4 +190,17 @@ export default function Home() {
       `}</style>
     </div>
   )
+};
+
+export const getStaticProps = async () => {
+  const files = fs.readdirSync ("posts");
+
+  return {
+    props: {
+      posts : files.map(filename=> filename.replace(".md",""))
+    }
+  }
 }
+
+
+export default Home;
